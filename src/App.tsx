f34eb81789cf405b41d3798fe5741a42d721d629
Sorriso337 <gabriel.santos@acovisa.com.br>
2024-09-04 20:46:49 -0300

    Criando base do corpo do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material'
 import { Header } from './components/header'
 
 function App() {
   return (
-    <>
+    <Box height='80%'>
       <Header />
-    </>
+      <Box sx={{ margin: 1, minHeight: '90vh', padding: 2, borderRadius: 4, border: '1px solid var(--Shapes-Border-card, #D8DFE6)' }}>
+      </Box>
+    </Box>
   )
 }
 
