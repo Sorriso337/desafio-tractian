3b84d429cfe863bf71a2122f6712b65e38453da9
Sorriso337 <gabriel.santos@acovisa.com.br>
2024-09-09 19:19:27 -0300

    Instalando e instanciando recoil

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { RecoilRoot } from 'recoil'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </StrictMode>,
 )
