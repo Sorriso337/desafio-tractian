ff1efe128d475d5c1a18e9c9a2a4c4b121247212
Sorriso337 <gabriel.santos@acovisa.com.br>
2024-09-09 18:26:50 -0300

    Criando BreadCrumbs e botões de sensores

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Breadcrumbs, Button, Typography } from '@mui/material'
 import { Header } from './components/header'
 
 function App() {
@@ -6,6 +6,21 @@ function App() {
     <Box height='80%'>
       <Header />
       <Box sx={{ margin: 1, minHeight: '90vh', padding: 2, borderRadius: 4, border: '1px solid var(--Shapes-Border-card, #D8DFE6)' }}>
+        <Box display='flex' justifyContent='space-between'>
+
+          <Breadcrumbs aria-label="breadcrumb">
+            <Typography fontWeight='bold' >Ativos</Typography>
+            <Typography >Apex Unit</Typography>
+          </Breadcrumbs>
+          <Box display='flex' justifyContent='center'>
+            <Button variant='outlined' size='small' style={{ marginRight: 16 }}>
+              Sensor de energia
+            </Button>
+            <Button variant='outlined' size='small' style={{ marginRight: 16 }}>
+              Crítico
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
