014c5de8e49400eafbc19e0ed49468fd0ef4a024
Sorriso337 <gabriel.santos@acovisa.com.br>
2024-09-09 22:24:25 -0300

    Retirando função não utilizada

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -8,14 +8,3 @@ export const buildTree = (parentId: string | null, items: Array<Asset | Location
             children: buildTree(item.id, items),
         }));
 };
-
-export const flattenTree = (tree: TreeNode[]) => {
-    let flatList: TreeNode[] = [];
-    tree.forEach(node => {
-        flatList.push(node);
-        if (node.children) {
-            flatList = flatList.concat(flattenTree(node.children));
-        }
-    });
-    return flatList;
-};
